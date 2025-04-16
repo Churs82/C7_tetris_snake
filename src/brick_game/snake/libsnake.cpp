69a647dbbf773d6c132dd1c586d74744b67b3398
Sarnori Sold <sarnoris@student.21-school.ru>
2025-04-16 14:52:08 +0300

    renamed old fsm

--- src/brick_game/snake/libsnake.cpp
+++ src/brick_game/snake/libsnake.cpp
@@ -1,19 +1,11 @@
-#include <sys/time.h>
-#include <time.h>
-
 #include "lib.h"
 #include "model.h"
 
-using s21::snake::model;
-
-static model* getModel() {
-  static std::unique_ptr<model> instance = s21::snake::model::factory::create();
-  return instance.get();
-}
-
 void userInput(UserAction_t action, bool hold) {
   if (!updateCurrentState().pause || action == Pause || action == Terminate)
-    getModel()->userAction(action);
+    s21::snake::model::instance::get()->userAction(action);
 }
 
-GameInfo_t updateCurrentState() { return getModel()->updateState(); }
+GameInfo_t updateCurrentState() {
+  return s21::snake::model::instance::get()->updateState();
+}
--- src/brick_game/snake/model.cpp
+++ src/brick_game/snake/model.cpp
@@ -1,32 +1,3 @@
 #include "model.h"
 
-namespace s21::snake {
-
-class Start_state : public State {
-  using Next_state = Spawn_state;
-
- public:
-  Start_state(){};
-  virtual void Enter() override {};
-  virtual void Exit() override { fsm_->TransitionTo<Next_state>(); };
-};
-
-class Spawn_state : public State {
-  using Next_state = Rotating_state;
-
- public:
-  Spawn_state(){};
-  virtual void Enter() override {
-
-  };
-
-  virtual void Exit() override { fsm_->TransitionTo<Next_state>(); };
-};
-
-class Exit_state : public State {
- public:
-  Exit_state(){};
-  virtual void Enter() override {};
-  virtual void Exit() override {};
-};
-}  // namespace s21::snake
+namespace s21::snake {}  // namespace s21::snake
