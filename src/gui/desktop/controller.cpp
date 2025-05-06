198d9cab6099a2375d1373eb1fa81b1e5398a016
Sarnori Sold <sarnoris@student.21-school.ru>
2025-05-06 22:37:32 +0300

    added controller from tetris to start with

---
+++ src/gui/desktop/controller.cpp
@@ -0,0 +1,59 @@
+#include "game.h"
+
+int main(void) {
+  WIN_INIT(30);
+  setlocale(LC_ALL, "");
+  print_overlay();
+  game_loop();
+  endwin();
+  return SUCCESS;
+}
+
+/**
+ * @brief The main game loop.
+ *
+ * Prints the game field and handles user input.
+ *
+ * The loop will break if the game field is null, indicating the game has ended.
+ *
+ * @return void
+ */
+void game_loop() {
+  intro();
+
+  GameInfo_t game_info = updateCurrentState();
+
+  while (game_info.field) {
+    print_board(game_info);
+
+    /* User input */
+    int signal = GET_USER_INPUT;
+    if (signal > -1) {
+      if (get_signal(signal) == Terminate) {
+        end_game();
+      } else {
+        userInput(get_signal(signal), false);
+      }
+    }
+
+    game_info = updateCurrentState();
+  }
+}
+
+void intro() {
+  print_intro();
+  int signal = -1;
+  while (get_signal(signal) != Start && get_signal(signal) != Terminate) {
+    signal = GET_USER_INPUT;
+  }
+  userInput(get_signal(signal), false);
+}
+
+void end_game() {
+  print_exit();
+  int signal = -1;
+  while (get_signal(signal) != Start && get_signal(signal) != Terminate) {
+    signal = GET_USER_INPUT;
+  }
+  userInput(get_signal(signal), false);
+}
---
+++ src/gui/desktop/controller.h
@@ -0,0 +1,11 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "lib.h"
+#include "view.h"
+
+void game_loop();
+void end_game();
+void intro();
+
+#endif
